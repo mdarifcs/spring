f193cbf245f91a90a7af6ca883a3c95566db70a1
mohammad arif <mdarif.cs@gmail.com>
2019-07-06 09:32:26 +0530

    Security for all URL

---
+++ spring-basic-security/src/main/java/com/example/security/config/SpringSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("arif").password("pass@1234").roles("ADMIN");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+	}
+	
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+}
